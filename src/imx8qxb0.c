nxp-imx/imx-mkimage

commit cbe4af1618ac2e30e0fa2da5142299fe20dc12a3

Add QM B0 support.
Add b0 target in QM/soc.mak
Also tweak the build container function for QX B0
in order to handle QM (Core resource id needs to be different).

Reviewed-by: Abel Vesa <abel.vesa@nxp.com>
Signed-off-by: Adriana Reus <adriana.reus@nxp.com>

src/imx8qxb0.c
@@ -54,8 +54,28 @@
 
 #define FIRST_CONTAINER_HEADER_LENGTH	0x400
 
-#define IMAGE_AP_DEFAULT_META		0x001355FC
-#define IMAGE_M4_DEFAULT_META		0x0004A516
+#define BOOT_IMG_META_MU_RID_SHIFT	10
+#define BOOT_IMG_META_PART_ID_SHIFT	20
+
+#define IMAGE_A35_DEFAULT_META		(PARTITION_ID_AP << BOOT_IMG_META_PART_ID_SHIFT | \
+					 SC_R_MU_0A << BOOT_IMG_META_MU_RID_SHIFT | \
+					 SC_R_A35_0)
+
+#define IMAGE_A53_DEFAULT_META		(PARTITION_ID_AP << BOOT_IMG_META_PART_ID_SHIFT | \
+					 SC_R_MU_0A << BOOT_IMG_META_MU_RID_SHIFT | \
+					 SC_R_A53_0)
+
+#define IMAGE_A72_DEFAULT_META		(PARTITION_ID_AP << BOOT_IMG_META_PART_ID_SHIFT | \
+					 SC_R_MU_0A << BOOT_IMG_META_MU_RID_SHIFT | \
+					 SC_R_A72_0)
+
+#define IMAGE_M4_0_DEFAULT_META		(PARTITION_ID_M4 << BOOT_IMG_META_PART_ID_SHIFT | \
+					 SC_R_M4_0_MU_1A << BOOT_IMG_META_MU_RID_SHIFT | \
+					 SC_R_M4_0_PID0)
+
+#define IMAGE_M4_1_DEFAULT_META		(PARTITION_ID_M4 << BOOT_IMG_META_PART_ID_SHIFT | \
+					 SC_R_M4_1_MU_1A << BOOT_IMG_META_MU_RID_SHIFT | \
+					 SC_R_M4_1_PID0)
 
 #define CONTAINER_IMAGE_ARRAY_START_OFFSET	0x2000
 
@@ -320,13 +340,20 @@ uint64_t read_dcd_offset(char *filename)
 	return offset;
 }
 
-void set_image_array_entry(flash_header_v3_t *container, option_type_t type, uint32_t offset,
-		uint32_t size, uint64_t dst, uint64_t entry, char *tmp_filename, bool dcd_skip)
+void set_image_array_entry(flash_header_v3_t *container, soc_type_t soc,
+		const image_t *image_stack, uint32_t offset,
+		uint32_t size, char *tmp_filename, bool dcd_skip)
 {
+	uint64_t entry = image_stack->entry;
+	uint64_t core = image_stack->ext;
+	uint32_t meta;
+	char *tmp_name = "";
+	option_type_t type = image_stack->option;
 	boot_img_t *img = &container->img[container->num_images];
+
+
 	img->offset = offset;  /* Is re-adjusted later */
 	img->size = size;
-	char *tmp_name = "";
 
 	set_image_hash(img, tmp_filename, IMAGE_HASH_ALGO_DEFAULT);
 
@@ -339,24 +366,44 @@ void set_image_array_entry(flash_header_v3_t *container, option_type_t type, uin
 		img->entry = 0x20000000;
 		break;
 	case AP:
+		if (soc == QX && core == CORE_CA35)
+			meta = IMAGE_A35_DEFAULT_META;
+		else if (soc == QM && core == CORE_CA53)
+			meta = IMAGE_A53_DEFAULT_META;
+		else if (soc == QM && core == CORE_CA72)
+			meta = IMAGE_A72_DEFAULT_META;
+		else {
+			fprintf(stderr, "Error: invalid AP core id: %" PRIi64 "\n", core);
+			exit(EXIT_FAILURE);
+		}
 		img->hab_flags |= IMG_TYPE_EXEC;
-		img->hab_flags |= CORE_CA35 << BOOT_IMG_FLAGS_CORE_SHIFT;
+		img->hab_flags |= core << BOOT_IMG_FLAGS_CORE_SHIFT;
 		tmp_name = "AP";
 		img->dst = entry;
 		img->entry = entry;
-		img->meta = IMAGE_AP_DEFAULT_META;
+		img->meta = meta;
 		break;
 	case M4:
+		if (core == 0) {
+			core = CORE_CM4_0;
+			meta = IMAGE_M4_0_DEFAULT_META;
+		} else if (core == 1) {
+			core = CORE_CM4_1;
+			meta = IMAGE_M4_1_DEFAULT_META;
+		} else {
+			fprintf(stderr, "Error: invalid m4 core id: %" PRIi64 "\n", core);
+			exit(EXIT_FAILURE);
+		}
 		img->hab_flags |= IMG_TYPE_EXEC;
-		img->hab_flags |= CORE_CM4_0 << BOOT_IMG_FLAGS_CORE_SHIFT;
+		img->hab_flags |= core << BOOT_IMG_FLAGS_CORE_SHIFT;
 		tmp_name = "M4";
 		img->dst = entry;
 		img->entry = entry;
-		img->meta = IMAGE_M4_DEFAULT_META;
+		img->meta = meta;
 		break;
 	case DATA:
 		img->hab_flags |= IMG_TYPE_DATA;
-		img->hab_flags |= CORE_CA35 << BOOT_IMG_FLAGS_CORE_SHIFT;
+		img->hab_flags |= core << BOOT_IMG_FLAGS_CORE_SHIFT;
 		tmp_name = "DATA";
 		img->dst = entry;
 		break;
@@ -437,7 +484,7 @@ int get_container_image_start_pos(image_t *image_stack, uint32_t align)
 	return file_off;
 }
 
-int build_container_qx_b0(uint32_t sector_size, uint32_t ivt_offset, char *out_file,
+int build_container_qx_qm_b0(soc_type_t soc, uint32_t sector_size, uint32_t ivt_offset, char *out_file,
 				bool emmc_fastboot, image_t *image_stack, bool dcd_skip)
 {
 	int file_off, ofd = -1;
@@ -460,7 +507,10 @@ int build_container_qx_b0(uint32_t sector_size, uint32_t ivt_offset, char *out_f
 		exit(EXIT_FAILURE);
 	}
 
-	fprintf(stdout, "Platform:\ti.MX8QXP B0\n");
+	if (soc == QX)
+		fprintf(stdout, "Platform:\ti.MX8QXP B0\n");
+	else if (soc == QM)
+		fprintf(stdout, "Platform:\ti.MX8QM B0\n");
 
 	set_imx_hdr_v3(&imx_header, dcd_len, ivt_offset, INITIAL_LOAD_ADDR_SCU_ROM, 0);
 	set_imx_hdr_v3(&imx_header, 0, ivt_offset, INITIAL_LOAD_ADDR_AP_ROM, 1);
@@ -483,11 +533,10 @@ int build_container_qx_b0(uint32_t sector_size, uint32_t ivt_offset, char *out_f
 			check_file(&sbuf, img_sp->filename);
 			tmp_filename = img_sp->filename;
 			set_image_array_entry(&imx_header.fhdr[container],
-						img_sp->option,
+						soc,
+						img_sp,
 						file_off,
 						ALIGN(sbuf.st_size, sector_size),
-						img_sp->dst,
-						img_sp->entry,
 						tmp_filename,
 						dcd_skip);
 			img_sp->src = file_off;

iMX8QM/soc.mak
@@ -116,6 +116,9 @@ flash_aprom_ddr: $(MKIMG) $(DCD_CFG) scfw_tcm.bin u-boot-atf.bin aprom_ddr.bin c
 flash_aprom_ddr_unsigned: $(MKIMG) $(DCD_CFG) scfw_tcm.bin u-boot-atf.bin aprom_ddr.bin csf_ap.bin
 	./$(MKIMG) -soc QM -c -dcd $(DCD_CFG) -scfw scfw_tcm.bin -ap aprom_ddr.bin a53 0x80000000 -c -ap u-boot-atf.bin a53 0x90000000 -out flash.bin
 
+flash_b0: $(MKIMG) $(DCD_CFG) ahab-container.img scfw_tcm.bin u-boot-atf.bin
+	./$(MKIMG) -soc QM -rev B0 -append ahab-container.img -c -scfw scfw_tcm.bin -ap u-boot-atf.bin a53 0x80000000 -out flash.bin
+
 
 nightly :
 	@rm -rf boot

src/mkimage_common.h
@@ -310,7 +310,7 @@ int build_container_qm(uint32_t sector_size, uint32_t ivt_offset, char * out_fil
 int build_container_qx(uint32_t sector_size, uint32_t ivt_offset, char * out_file,
                 bool emmc_fastboot, image_t* image_stack);
 
-int build_container_qx_b0(uint32_t sector_size, uint32_t ivt_offset, char * out_file,
+int build_container_qx_qm_b0(soc_type_t soc, uint32_t sector_size, uint32_t ivt_offset, char * out_file,
                 bool emmc_fastboot, image_t* image_stack, bool dcd_skip);
 
 

src/mkimage_imx8.c
@@ -575,7 +575,7 @@ int main(int argc, char **argv)
                                 fprintf(stdout, "SOC: %s \n",optarg);
                                 break;
                         case 'r':
-				if(soc == QX) {
+				if(soc == QX || soc == QM) {
 				  if(strcmp(optarg, "A0") == 0)
 				    rev = A0;
 				  else if(strcmp(optarg, "B0") == 0) {
@@ -772,13 +772,16 @@ int main(int argc, char **argv)
 		fprintf(stdout, "ivt_offset:\t%d\n", ivt_offset);
 		fprintf(stdout, "rev:\t%d\n", rev);
 		if (rev == B0)
-			build_container_qx_b0(sector_size, ivt_offset, ofname, emmc_fastboot, (image_t *) param_stack, dcd_skip);
+			build_container_qx_qm_b0(soc, sector_size, ivt_offset, ofname, emmc_fastboot, (image_t *) param_stack, dcd_skip);
 		else
 			build_container_qx(sector_size, ivt_offset, ofname, emmc_fastboot, (image_t *) param_stack);
 
 		break;
           case QM:
-              build_container_qm(sector_size, ivt_offset, ofname, emmc_fastboot, (image_t *) param_stack);
+		if (rev == B0)
+			build_container_qx_qm_b0(soc, sector_size, ivt_offset, ofname, emmc_fastboot, (image_t *) param_stack, dcd_skip);
+		else
+			build_container_qm(sector_size, ivt_offset, ofname, emmc_fastboot, (image_t *) param_stack);
               break;
           default:
               fprintf(stderr, " unrecognized SOC defined");

commit 1ea995c59295ed558efd4fd153bc864c139eb820

Fix hab flags for data type image
There is no ap core specified for data type images.
Only qxp has data-type images so replace the core ap
with CORE_CA35.

Fixes: cbe4af1618ac ('Add QM B0 support')
Signed-off-by: Adriana Reus <adriana.reus@nxp.com>

src/imx8qxb0.c
@@ -403,7 +403,7 @@ void set_image_array_entry(flash_header_v3_t *container, soc_type_t soc,
 		break;
 	case DATA:
 		img->hab_flags |= IMG_TYPE_DATA;
-		img->hab_flags |= core << BOOT_IMG_FLAGS_CORE_SHIFT;
+		img->hab_flags |= CORE_CA35 << BOOT_IMG_FLAGS_CORE_SHIFT;
 		tmp_name = "DATA";
 		img->dst = entry;
 		break;
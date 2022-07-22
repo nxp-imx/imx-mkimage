nxp-imx/imx-mkimage

commit 57a8eda0812ee052943410a48eea3912bd9d1f75

makefile: compile in the specified directory
flash.bin can be compiled in the specified directory when using "O=xxx".

Examples:
i.MX8MM:
make SOC=iMX8MM O=buildout_imx8mm flash_evk
make SOC=iMX8MM O=buildout_imx8mm clean
i.MX93:
make SOC=iMX9 O=buildout_imx9 flash_singleboot
make SOC=iMX9 O=buildout_imx9 clean

Reviewed-by: Ye Li <ye.li@nxp.com>
Signed-off-by: Alice Guo <alice.guo@nxp.com>

iMX8M/soc.mak
@@ -82,10 +82,10 @@ endif
 
 FW_DIR = imx-boot/imx-boot-tools/$(PLAT)
 
-$(MKIMG): mkimage_imx8.c
+$(MKIMG): ../$(SOC_DIR)/mkimage_imx8.c
 	@echo "PLAT="$(PLAT) "HDMI="$(HDMI)
 	@echo "Compiling mkimage_imx8"
-	$(CC) $(CFLAGS) mkimage_imx8.c -o $(MKIMG) $(BUILD_LDFLAGS) -lz
+	$(CC) $(CFLAGS) ../$(SOC_DIR)/mkimage_imx8.c -o $(MKIMG) $(BUILD_LDFLAGS) -lz
 
 lpddr4_imem_1d = lpddr4_pmu_train_1d_imem$(LPDDR_FW_VERSION).bin
 lpddr4_dmem_1d = lpddr4_pmu_train_1d_dmem$(LPDDR_FW_VERSION).bin
@@ -148,8 +148,8 @@ u-boot.itb: $(dtb)
 	./$(PAD_IMAGE) $(TEE)
 	./$(PAD_IMAGE) bl31.bin
 	./$(PAD_IMAGE) u-boot-nodtb.bin $(dtb)
-	BL32=$(TEE) DEK_BLOB_LOAD_ADDR=$(DEK_BLOB_LOAD_ADDR) TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ATF_LOAD_ADDR=$(ATF_LOAD_ADDR) ./mkimage_fit_atf.sh $(dtb) > u-boot.its
-	./mkimage_uboot -E -p 0x3000 -f u-boot.its u-boot.itb
+	BL32=$(TEE) DEK_BLOB_LOAD_ADDR=$(DEK_BLOB_LOAD_ADDR) TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ATF_LOAD_ADDR=$(ATF_LOAD_ADDR) ../$(SOC_DIR)/mkimage_fit_atf.sh $(dtb) > u-boot.its
+	../$(SOC_DIR)/mkimage_uboot -E -p 0x3000 -f u-boot.its u-boot.itb
 	@rm -f u-boot.its $(dtb)
 
 dtb_ddr3l = valddr3l.dtb
@@ -160,8 +160,8 @@ u-boot-ddr3l.itb: $(dtb_ddr3l)
 	./$(PAD_IMAGE) $(TEE)
 	./$(PAD_IMAGE) bl31.bin
 	./$(PAD_IMAGE) u-boot-nodtb.bin $(dtb_ddr3l)
-	DEK_BLOB_LOAD_ADDR=$(DEK_BLOB_LOAD_ADDR) TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ATF_LOAD_ADDR=$(ATF_LOAD_ADDR) ./mkimage_fit_atf.sh $(dtb_ddr3l) > u-boot-ddr3l.its
-	./mkimage_uboot -E -p 0x3000 -f u-boot-ddr3l.its u-boot-ddr3l.itb
+	DEK_BLOB_LOAD_ADDR=$(DEK_BLOB_LOAD_ADDR) TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ATF_LOAD_ADDR=$(ATF_LOAD_ADDR) ../$(SOC_DIR)/mkimage_fit_atf.sh $(dtb_ddr3l) > u-boot-ddr3l.its
+	../$(SOC_DIR)/mkimage_uboot -E -p 0x3000 -f u-boot-ddr3l.its u-boot-ddr3l.itb
 	@rm -f u-boot.its $(dtb_ddr3l)
 
 dtb_ddr3l_evk = evkddr3l.dtb
@@ -172,8 +172,8 @@ u-boot-ddr3l-evk.itb: $(dtb_ddr3l_evk)
 	./$(PAD_IMAGE) $(TEE)
 	./$(PAD_IMAGE) bl31.bin
 	./$(PAD_IMAGE) u-boot-nodtb.bin $(dtb_ddr3l_evk)
-	DEK_BLOB_LOAD_ADDR=$(DEK_BLOB_LOAD_ADDR) TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ATF_LOAD_ADDR=$(ATF_LOAD_ADDR) ./mkimage_fit_atf.sh $(dtb_ddr3l_evk) > u-boot-ddr3l-evk.its
-	./mkimage_uboot -E -p 0x3000 -f u-boot-ddr3l-evk.its u-boot-ddr3l-evk.itb
+	DEK_BLOB_LOAD_ADDR=$(DEK_BLOB_LOAD_ADDR) TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ATF_LOAD_ADDR=$(ATF_LOAD_ADDR) ../$(SOC_DIR)/mkimage_fit_atf.sh $(dtb_ddr3l_evk) > u-boot-ddr3l-evk.its
+	../$(SOC_DIR)/mkimage_uboot -E -p 0x3000 -f u-boot-ddr3l-evk.its u-boot-ddr3l-evk.itb
 	@rm -f u-boot.its $(dtb_ddr3l_evk)
 
 dtb_ddr4 = valddr4.dtb
@@ -184,8 +184,8 @@ u-boot-ddr4.itb: $(dtb_ddr4)
 	./$(PAD_IMAGE) $(TEE)
 	./$(PAD_IMAGE) bl31.bin
 	./$(PAD_IMAGE) u-boot-nodtb.bin $(dtb_ddr4)
-	DEK_BLOB_LOAD_ADDR=$(DEK_BLOB_LOAD_ADDR) TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ATF_LOAD_ADDR=$(ATF_LOAD_ADDR) ./mkimage_fit_atf.sh $(dtb_ddr4) > u-boot-ddr4.its
-	./mkimage_uboot -E -p 0x3000 -f u-boot-ddr4.its u-boot-ddr4.itb
+	DEK_BLOB_LOAD_ADDR=$(DEK_BLOB_LOAD_ADDR) TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ATF_LOAD_ADDR=$(ATF_LOAD_ADDR) ../$(SOC_DIR)/mkimage_fit_atf.sh $(dtb_ddr4) > u-boot-ddr4.its
+	../$(SOC_DIR)/mkimage_uboot -E -p 0x3000 -f u-boot-ddr4.its u-boot-ddr4.itb
 	@rm -f u-boot.its $(dtb_ddr4)
 
 dtb_ddr4_evk = evkddr4.dtb
@@ -196,8 +196,8 @@ u-boot-ddr4-evk.itb: $(dtb_ddr4_evk)
 	./$(PAD_IMAGE) $(TEE)
 	./$(PAD_IMAGE) bl31.bin
 	./$(PAD_IMAGE) u-boot-nodtb.bin $(dtb_ddr4_evk)
-	DEK_BLOB_LOAD_ADDR=$(DEK_BLOB_LOAD_ADDR) TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ATF_LOAD_ADDR=$(ATF_LOAD_ADDR) ./mkimage_fit_atf.sh $(dtb_ddr4_evk) > u-boot-ddr4-evk.its
-	./mkimage_uboot -E -p 0x3000 -f u-boot-ddr4-evk.its u-boot-ddr4-evk.itb
+	DEK_BLOB_LOAD_ADDR=$(DEK_BLOB_LOAD_ADDR) TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ATF_LOAD_ADDR=$(ATF_LOAD_ADDR) ../$(SOC_DIR)/mkimage_fit_atf.sh $(dtb_ddr4_evk) > u-boot-ddr4-evk.its
+	../$(SOC_DIR)/mkimage_uboot -E -p 0x3000 -f u-boot-ddr4-evk.its u-boot-ddr4-evk.itb
 	@rm -f u-boot.its $(dtb_ddr4_evk)
 
 ifeq ($(HDMI),yes)
@@ -288,21 +288,21 @@ print_fit_hab: u-boot-nodtb.bin bl31.bin $(dtb)
 	./$(PAD_IMAGE) $(TEE)
 	./$(PAD_IMAGE) bl31.bin
 	./$(PAD_IMAGE) u-boot-nodtb.bin $(dtb)
-	TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ATF_LOAD_ADDR=$(ATF_LOAD_ADDR) VERSION=$(VERSION) ./print_fit_hab.sh $(PRINT_FIT_HAB_OFFSET) $(dtb)
+	TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ATF_LOAD_ADDR=$(ATF_LOAD_ADDR) VERSION=$(VERSION) ../$(SOC_DIR)/print_fit_hab.sh $(PRINT_FIT_HAB_OFFSET) $(dtb)
 	@rm -f $(dtb)
 
 print_fit_hab_ddr4: u-boot-nodtb.bin bl31.bin $(dtb_ddr4_evk)
 	./$(PAD_IMAGE) $(TEE)
 	./$(PAD_IMAGE) bl31.bin
 	./$(PAD_IMAGE) u-boot-nodtb.bin $(dtb_ddr4_evk)
-	TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ATF_LOAD_ADDR=$(ATF_LOAD_ADDR) VERSION=$(VERSION) ./print_fit_hab.sh $(PRINT_FIT_HAB_OFFSET) $(dtb_ddr4_evk)
+	TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ATF_LOAD_ADDR=$(ATF_LOAD_ADDR) VERSION=$(VERSION) ../$(SOC_DIR)/print_fit_hab.sh $(PRINT_FIT_HAB_OFFSET) $(dtb_ddr4_evk)
 	@rm -f $(dtb_ddr4_evk)
 
 print_fit_hab_flexspi: u-boot-nodtb.bin bl31.bin $(dtb)
 	./$(PAD_IMAGE) $(TEE)
 	./$(PAD_IMAGE) bl31.bin
 	./$(PAD_IMAGE) u-boot-nodtb.bin $(dtb)
-	TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ATF_LOAD_ADDR=$(ATF_LOAD_ADDR) VERSION=$(VERSION) BOOT_DEV="flexspi" ./print_fit_hab.sh $(PRINT_FIT_HAB_OFFSET) $(dtb)
+	TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ATF_LOAD_ADDR=$(ATF_LOAD_ADDR) VERSION=$(VERSION) BOOT_DEV="flexspi" ../$(SOC_DIR)/print_fit_hab.sh $(PRINT_FIT_HAB_OFFSET) $(dtb)
 	@rm -f $(dtb)
 
 nightly :

iMX8DXL/soc.mak
@@ -164,37 +164,37 @@ extract: $(MKIMG) flash.bin
 	./$(MKIMG) -soc DXL -rev A0 -extract flash.bin
 
 
-ifneq ($(wildcard scripts/misc.mak),)
+ifneq ($(wildcard ../$(SOC_DIR)/scripts/misc.mak),)
 $(info include misc.mak)
-include scripts/misc.mak
+include ../$(SOC_DIR)/scripts/misc.mak
 endif
 
-ifneq ($(wildcard scripts/m4.mak),)
+ifneq ($(wildcard ../$(SOC_DIR)/scripts/m4.mak),)
 $(info include m4.mak)
-include scripts/m4.mak
+include ../$(SOC_DIR)/scripts/m4.mak
 endif
 
-ifneq ($(wildcard scripts/android.mak),)
+ifneq ($(wildcard ../$(SOC_DIR)/scripts/android.mak),)
 $(info include android.mak)
-include scripts/android.mak
+include ../$(SOC_DIR)/scripts/android.mak
 endif
 
-ifneq ($(wildcard scripts/test.mak),)
+ifneq ($(wildcard ../$(SOC_DIR)/scripts/test.mak),)
 $(info include test.mak)
-include scripts/test.mak
+include ../$(SOC_DIR)/scripts/test.mak
 endif
 
-ifneq ($(wildcard scripts/autobuild.mak),)
+ifneq ($(wildcard ../$(SOC_DIR)/scripts/autobuild.mak),)
 $(info include autobuild.mak)
-include scripts/autobuild.mak
+include ../$(SOC_DIR)/scripts/autobuild.mak
 endif
 
-ifneq ($(wildcard scripts/rev_a.mak),)
+ifneq ($(wildcard ../$(SOC_DIR)/scripts/rev_a.mak),)
 $(info include rev_a.mak)
-include scripts/rev_a.mak
+include ../$(SOC_DIR)/scripts/rev_a.mak
 endif
 
-ifneq ($(wildcard scripts/alias.mak),)
+ifneq ($(wildcard ../$(SOC_DIR)/scripts/alias.mak),)
 $(info include alias.mak)
-include scripts/alias.mak
+include ../$(SOC_DIR)/scripts/alias.mak
 endif

iMX8QM/soc.mak
@@ -53,7 +53,7 @@ u-boot-atf.itb: u-boot-hash.bin bl31.bin
 	fi
 	./$(PAD_IMAGE) bl31.bin
 	./$(PAD_IMAGE) u-boot-hash.bin
-	./mkimage_fit_atf.sh > u-boot.its;
+	../$(SOC_DIR)/mkimage_fit_atf.sh > u-boot.its;
 	./mkimage_uboot -E -p 0x3000 -f u-boot.its u-boot-atf.itb;
 	@rm -f u-boot.its
 
@@ -191,32 +191,32 @@ parse_container: $(MKIMG) flash.bin
 extract: $(MKIMG) flash.bin
 	./$(MKIMG) -soc QM -rev B0 -extract flash.bin
 
-ifneq ($(wildcard scripts/misc.mak),)
+ifneq ($(wildcard ../$(SOC_DIR)/scripts/misc.mak),)
 $(info include misc.mak)
-include scripts/misc.mak
+include ../$(SOC_DIR)/scripts/misc.mak
 endif
 
-ifneq ($(wildcard scripts/m4.mak),)
+ifneq ($(wildcard ../$(SOC_DIR)/scripts/m4.mak),)
 $(info include m4.mak)
-include scripts/m4.mak
+include ../$(SOC_DIR)/scripts/m4.mak
 endif
 
-ifneq ($(wildcard scripts/android.mak),)
+ifneq ($(wildcard ../$(SOC_DIR)/scripts/android.mak),)
 $(info include android.mak)
-include scripts/android.mak
+include ../$(SOC_DIR)/scripts/android.mak
 endif
 
-ifneq ($(wildcard scripts/test.mak),)
+ifneq ($(wildcard ../$(SOC_DIR)/scripts/test.mak),)
 $(info include test.mak)
-include scripts/test.mak
+include ../$(SOC_DIR)/scripts/test.mak
 endif
 
-ifneq ($(wildcard scripts/autobuild.mak),)
+ifneq ($(wildcard ../$(SOC_DIR)/scripts/autobuild.mak),)
 $(info include autobuild.mak)
-include scripts/autobuild.mak
+include ../$(SOC_DIR)/scripts/autobuild.mak
 endif
 
-ifneq ($(wildcard scripts/alias.mak),)
+ifneq ($(wildcard ../$(SOC_DIR)/scripts/alias.mak),)
 $(info include alias.mak)
-include scripts/alias.mak
+include ../$(SOC_DIR)/scripts/alias.mak
 endif

iMX8QX/soc.mak
@@ -41,7 +41,7 @@ u-boot-atf.bin: u-boot-hash.bin bl31.bin
 u-boot-atf.itb: u-boot-hash.bin bl31.bin
 	./$(PAD_IMAGE) bl31.bin
 	./$(PAD_IMAGE) u-boot-hash.bin
-	TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ./mkimage_fit_atf.sh > u-boot.its;
+	TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ../$(SOC_DIR)/mkimage_fit_atf.sh > u-boot.its;
 	./mkimage_uboot -E -p 0x3000 -f u-boot.its u-boot-atf.itb;
 	@rm -f u-boot.its
 
@@ -148,32 +148,32 @@ extract: $(MKIMG) flash.bin
 	./$(MKIMG) -soc QX -rev B0 -extract flash.bin
 
 
-ifneq ($(wildcard scripts/misc.mak),)
+ifneq ($(wildcard ../$(SOC_DIR)/scripts/misc.mak),)
 $(info include misc.mak)
-include scripts/misc.mak
+include ../$(SOC_DIR)/scripts/misc.mak
 endif
 
-ifneq ($(wildcard scripts/m4.mak),)
+ifneq ($(wildcard ../$(SOC_DIR)/scripts/m4.mak),)
 $(info include m4.mak)
-include scripts/m4.mak
+include ../$(SOC_DIR)/scripts/m4.mak
 endif
 
-ifneq ($(wildcard scripts/android.mak),)
+ifneq ($(wildcard ../$(SOC_DIR)/scripts/android.mak),)
 $(info include android.mak)
-include scripts/android.mak
+include ../$(SOC_DIR)/scripts/android.mak
 endif
 
-ifneq ($(wildcard scripts/test.mak),)
+ifneq ($(wildcard ../$(SOC_DIR)/scripts/test.mak),)
 $(info include test.mak)
-include scripts/test.mak
+include ../$(SOC_DIR)/scripts/test.mak
 endif
 
-ifneq ($(wildcard scripts/autobuild.mak),)
+ifneq ($(wildcard ../$(SOC_DIR)/scripts/autobuild.mak),)
 $(info include autobuild.mak)
-include scripts/autobuild.mak
+include ../$(SOC_DIR)/scripts/autobuild.mak
 endif
 
-ifneq ($(wildcard scripts/alias.mak),)
+ifneq ($(wildcard ../$(SOC_DIR)/scripts/alias.mak),)
 $(info include alias.mak)
-include scripts/alias.mak
+include ../$(SOC_DIR)/scripts/alias.mak
 endif

Makefile
@@ -20,7 +20,11 @@ vpath $(INCLUDE)
 
 .DEFAULT:
 	@$(MAKE) -s --no-print-directory bin
-	@$(MAKE) --no-print-directory -C $(SOC_DIR) -f soc.mak $@
+ifdef O
+	@$(MAKE) --no-print-directory SOC_DIR=$(SOC_DIR) -C $(O) -f ../$(SOC_DIR)/soc.mak $@
+else
+	@$(MAKE) --no-print-directory SOC_DIR=$(SOC_DIR) -C $(SOC_DIR) -f soc.mak $@
+endif
 
 #print out usage as the default target
 all: $(MKIMG) help
@@ -28,6 +32,9 @@ all: $(MKIMG) help
 clean:
 	@rm -f $(MKIMG)
 	@rm -f src/build_info.h
+ifdef O
+	@$(MAKE) --no-print-directory -C $(O) -f ../$(SOC_DIR)/soc.mak clean
+endif
 	@$(MAKE) --no-print-directory -C iMX8QM -f soc.mak clean
 	@$(MAKE) --no-print-directory -C iMX8QX -f soc.mak  clean
 	@$(MAKE) --no-print-directory -C iMX8DXL -f soc.mak clean

commit 96dba6b44edd50aac269b1f0e0c736e77d0d6c3e

iMX8M: correct the path of mkimage_uboot
mkimage_uboot should be in the folder to which flash.bin is output.

Fixes: 57a8eda0812e ("makefile: compile in the specified directory")

Reviewed-by: Ye Li <ye.li@nxp.com>
Signed-off-by: Alice Guo <alice.guo@nxp.com>

iMX8M/soc.mak
@@ -149,7 +149,7 @@ u-boot.itb: $(dtb)
 	./$(PAD_IMAGE) bl31.bin
 	./$(PAD_IMAGE) u-boot-nodtb.bin $(dtb)
 	BL32=$(TEE) DEK_BLOB_LOAD_ADDR=$(DEK_BLOB_LOAD_ADDR) TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ATF_LOAD_ADDR=$(ATF_LOAD_ADDR) ../$(SOC_DIR)/mkimage_fit_atf.sh $(dtb) > u-boot.its
-	../$(SOC_DIR)/mkimage_uboot -E -p 0x3000 -f u-boot.its u-boot.itb
+	./mkimage_uboot -E -p 0x3000 -f u-boot.its u-boot.itb
 	@rm -f u-boot.its $(dtb)
 
 dtb_ddr3l = valddr3l.dtb
@@ -161,7 +161,7 @@ u-boot-ddr3l.itb: $(dtb_ddr3l)
 	./$(PAD_IMAGE) bl31.bin
 	./$(PAD_IMAGE) u-boot-nodtb.bin $(dtb_ddr3l)
 	DEK_BLOB_LOAD_ADDR=$(DEK_BLOB_LOAD_ADDR) TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ATF_LOAD_ADDR=$(ATF_LOAD_ADDR) ../$(SOC_DIR)/mkimage_fit_atf.sh $(dtb_ddr3l) > u-boot-ddr3l.its
-	../$(SOC_DIR)/mkimage_uboot -E -p 0x3000 -f u-boot-ddr3l.its u-boot-ddr3l.itb
+	./mkimage_uboot -E -p 0x3000 -f u-boot-ddr3l.its u-boot-ddr3l.itb
 	@rm -f u-boot.its $(dtb_ddr3l)
 
 dtb_ddr3l_evk = evkddr3l.dtb
@@ -173,7 +173,7 @@ u-boot-ddr3l-evk.itb: $(dtb_ddr3l_evk)
 	./$(PAD_IMAGE) bl31.bin
 	./$(PAD_IMAGE) u-boot-nodtb.bin $(dtb_ddr3l_evk)
 	DEK_BLOB_LOAD_ADDR=$(DEK_BLOB_LOAD_ADDR) TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ATF_LOAD_ADDR=$(ATF_LOAD_ADDR) ../$(SOC_DIR)/mkimage_fit_atf.sh $(dtb_ddr3l_evk) > u-boot-ddr3l-evk.its
-	../$(SOC_DIR)/mkimage_uboot -E -p 0x3000 -f u-boot-ddr3l-evk.its u-boot-ddr3l-evk.itb
+	./mkimage_uboot -E -p 0x3000 -f u-boot-ddr3l-evk.its u-boot-ddr3l-evk.itb
 	@rm -f u-boot.its $(dtb_ddr3l_evk)
 
 dtb_ddr4 = valddr4.dtb
@@ -185,7 +185,7 @@ u-boot-ddr4.itb: $(dtb_ddr4)
 	./$(PAD_IMAGE) bl31.bin
 	./$(PAD_IMAGE) u-boot-nodtb.bin $(dtb_ddr4)
 	DEK_BLOB_LOAD_ADDR=$(DEK_BLOB_LOAD_ADDR) TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ATF_LOAD_ADDR=$(ATF_LOAD_ADDR) ../$(SOC_DIR)/mkimage_fit_atf.sh $(dtb_ddr4) > u-boot-ddr4.its
-	../$(SOC_DIR)/mkimage_uboot -E -p 0x3000 -f u-boot-ddr4.its u-boot-ddr4.itb
+	./mkimage_uboot -E -p 0x3000 -f u-boot-ddr4.its u-boot-ddr4.itb
 	@rm -f u-boot.its $(dtb_ddr4)
 
 dtb_ddr4_evk = evkddr4.dtb
@@ -197,7 +197,7 @@ u-boot-ddr4-evk.itb: $(dtb_ddr4_evk)
 	./$(PAD_IMAGE) bl31.bin
 	./$(PAD_IMAGE) u-boot-nodtb.bin $(dtb_ddr4_evk)
 	DEK_BLOB_LOAD_ADDR=$(DEK_BLOB_LOAD_ADDR) TEE_LOAD_ADDR=$(TEE_LOAD_ADDR) ATF_LOAD_ADDR=$(ATF_LOAD_ADDR) ../$(SOC_DIR)/mkimage_fit_atf.sh $(dtb_ddr4_evk) > u-boot-ddr4-evk.its
-	../$(SOC_DIR)/mkimage_uboot -E -p 0x3000 -f u-boot-ddr4-evk.its u-boot-ddr4-evk.itb
+	./mkimage_uboot -E -p 0x3000 -f u-boot-ddr4-evk.its u-boot-ddr4-evk.itb
 	@rm -f u-boot.its $(dtb_ddr4_evk)
 
 ifeq ($(HDMI),yes)